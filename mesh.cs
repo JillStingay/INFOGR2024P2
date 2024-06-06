dada290af72cdedca69ef661e453a88b94dd085b
JillStingray <jillpagee@gmail.com>
2024-06-06 16:53:10 +0200

    added basics of a node class

--- mesh.cs
+++ mesh.cs
@@ -16,7 +16,10 @@ namespace Template
         int vertexBufferId;                     // vertex buffer object (VBO) for vertex data
         int triangleBufferId;                   // element buffer object (EBO) for triangle vertex indices
         int quadBufferId;                       // element buffer object (EBO) for quad vertex indices (not in Modern OpenGL)
-
+        public Matrix4 modelMatrix { get; set; }
+        
+        
+        
         // constructor
         public Mesh(string filename)
         {
---
+++ node.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Mathematics;
+
+namespace Template;
+
+public class node
+{
+    public Matrix4 objectToParent { get; set; }
+    public Mesh mesh { get; set; }
+    //List<node> { }
+
+    public void Render(Matrix4 cameraMatrix)
+    {
+        Matrix4 worldSpace = Matrix4.Mult(mesh.modelMatrix, objectToParent);
+        Matrix4 modelView = Matrix4.Mult(worldSpace, cameraMatrix);
+    }
+}
