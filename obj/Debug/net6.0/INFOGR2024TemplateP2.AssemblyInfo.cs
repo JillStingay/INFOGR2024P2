ab0d9a44763165ce7888d36ad768e8802d042805
Tim van Schie <timvanschie1999@gmail.com>
2024-06-27 13:59:03 +0200

    made a sort of nice schene

--- obj/Debug/net6.0/INFOGR2024TemplateP2.AssemblyInfo.cs
+++ obj/Debug/net6.0/INFOGR2024TemplateP2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFOGR2024TemplateP2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8228624f8ebde641062c67a43abd766b95cdfc48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c212b85a859eaca31bd57b8ae11e8425f30f330")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFOGR2024TemplateP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFOGR2024TemplateP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
